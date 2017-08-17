b0b26f695405e498060a3e84ef8485c7c450d7e1
bara-alhamidi <bara119911@gmail.com>
2017-08-17 06:55:50 -0700

    Add files via upload

---
+++ get_ip.py
@@ -0,0 +1,19 @@
+import socket
+# written by Albara'a Alhamidi
+print("      ####################################")
+print("      #   Thank you for work my scrip ❤  #")
+print("      #       instgram :   _.brbr        #")
+print("      ####################################")
+
+def get_ip(website):
+    try:
+        ips=socket.gethostbyname(website)
+    except IOError:
+         print("Error URL failed, Try again ")
+         get_ip(website=input("Enter website again : "))
+    else:
+        print("IP website is {}".format(ips))
+get_ip(website=input("Enter website only https or http : "))
+
+
+
